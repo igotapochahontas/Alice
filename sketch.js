873be38423a7001b3a8288a2de95d960fe708bf0
igotapochahontas <igotapochahontas@yahoo.com>
2019-07-09 15:17:59 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -17,33 +17,33 @@ function setup() {
   bot.loadFile("brain.rive", brainReady, brainError);
 
   function brainReady() {
-    window.alert('Chatbot ready!');
+    alert('Chatbot ready!');
     bot.sortReplies();
   }
 
   function brainError() {
-    window.alert('Chatbot error!')
+    alert('Chatbot error!')
   }
 
 
-  let button = select('#submit');
-   let user_input = select('#user_input');
-   let output = select('#output');
+  // let button = select('#submit');
+  // let user_input = select('#user_input');
+  // let output = select('#output');
 
-   button.mousePressed(chat);
+  // button.mousePressed(chat);
 
   function gotSpeech() {
     if (speechRec.resultValue) {
       let input = speechRec.resultString;
-      user_input.value(input);
+      //user_input.value(input);
       let reply = bot.reply("local-user", input);
       speech.speak(reply);
       output.html(reply);
     }
   }
 
-   function chat() {
-    let input = user_input.value();
-  }
+  // function chat() {
+  //   let input = user_input.value();
+  // }
 
 }
